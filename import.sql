6be613a0ef5eaf460be7ece63a381270095574aa
Saravanan-246 <saravananmurugaveni@gmail.com>
2026-09-22 23:56:06 +0530

    Add analytics pipeline and dashboard

--- import.sql
+++
@@ -1,7 +0,0 @@
-USE food_delivery_analytics;
-LOAD DATA LOCAL INFILE 'C:/Users/ADMIN/Desktop/lathii/Food-Delivery-Analytics/data/processed/cleaned_food_delivery.csv'
-INTO TABLE food_delivery
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-IGNORE 1 LINES;
-
